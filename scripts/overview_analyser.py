3641a7279a50c7e24305a0453438ffc4de78b04e
GetieBalew24 <getiebalew00@gmail.com>
2024-12-19 23:13:56 +0300

    plot top 10 handsets

--- scripts/overview_analyser.py
+++ scripts/overview_analyser.py
@@ -16,3 +16,15 @@ class UserOverviewAnalysis:
         top_10_handsets=handset_counts.head(10)
         print("Top 10 Handsets Used by Customers:")
         return top_10_handsets.to_frame().reset_index()
+    
+    def plot_top_10_handsets(self, df):
+        handset_counts = df['Handset Type'].value_counts()
+        top_10_handsets = handset_counts.head(10)
+
+        # Plot the top 10 handsets
+        top_10_handsets.plot(kind='bar', legend=False, color='skyblue')
+        plt.title('Top 10 Handsets Used by Customers')
+        plt.xlabel('Handset Type')
+        plt.ylabel('Number of Users')
+        plt.xticks(rotation=45, ha='right')
+        plt.show()
