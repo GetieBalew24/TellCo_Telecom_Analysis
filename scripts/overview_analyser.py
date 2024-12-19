bfc346ea861650fbf1a72372a0c4698f8176322c
GetieBalew24 <getiebalew00@gmail.com>
2024-12-19 23:39:20 +0300

    Top 3 handset manufacturers

--- scripts/overview_analyser.py
+++ scripts/overview_analyser.py
@@ -28,3 +28,34 @@ class UserOverviewAnalysis:
         plt.ylabel('Number of Users')
         plt.xticks(rotation=45, ha='right')
         plt.show()
+        
+    def top_3_handset_manufacturers(self, df):
+        """
+        Plots the top 3 handset manufacturers based on the number of users.
+
+        Args:
+        df (pd.DataFrame): The input DataFrame containing handset information with a column 'Handset Manufacturer'.
+
+        Returns:
+        pd.DataFrame: A DataFrame with the top 3 handset manufacturers and their user counts.
+        """
+        # Count occurrences of each handset manufacturer
+        handset_manufacturer_counts = df['Handset Manufacturer'].value_counts()
+
+        # Get top 3 handset manufacturers
+        top_3_handset_manufacturers = handset_manufacturer_counts.head(3)
+        top_3_handset_manufacturers.to_frame().reset_index()
+        # Print the top 3 handset manufacturers
+        return top_3_handset_manufacturers
+    def plot_top_3_handset_manufacturers(self,df):
+        handset_manufacturer_counts=df['Handset Manufacturer'].value_counts()
+        # Get top 3 handset manufacturers
+        top_3_handset_manufacturers = handset_manufacturer_counts.head(3)
+        
+        # Plot the top 3 handset manufacturers
+        top_3_handset_manufacturers.plot(kind='bar', legend=False, color='skyblue')
+        plt.title('Top 3 Handset Manufacturers')
+        plt.xlabel('Handset Manufacturers')
+        plt.ylabel('Number of Users')
+        plt.xticks(rotation=45, ha='right')
+        plt.show()
